35484351ea275339b7e598c30ef524ffa7a9b001
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-07-28 11:31:08 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,21 +1,39 @@
 function solution(balls, share) { 
-    let ja = 1;
-    let aa = balls-share;
-    let a = 1;
-    let b = 1;
+
+    // for문 풀이
+//         let ja = 1;
+//     let aa = balls-share;
+//     let a = 1;
+//     let b = 1;
     
-     // ja
-     for(i=balls; i>=1; i--){
-        ja *= i;
-    }
-     // a
-    for(j=aa; j>=1; j--){
-        a *= j;
-    }
-     // b
-    for(k=share; k>=1; k--){
-        b *= k;
+//      // ja
+//      for(i=balls; i>=1; i--){
+//         ja *= i;
+//     }
+//      // a
+//     for(j=aa; j>=1; j--){
+//         a *= j;
+//     }
+//      // b
+//     for(k=share; k>=1; k--){
+//         b *= k;
+//     }    
+//     return  Math.round(ja / (a * b));
+    
+    //  재귀함수 풀이 - 몇몇 테스트가 재귀함수가 더 빠르다
+    let result = 0;
+    function factorial(n){
+        if (n == 0 || n == 1) {
+          return 1;
+        } else {
+          return n * factorial(n - 1)
+        }   
+    }     
+    if(balls === share){
+        return 1
     }    
-    return  Math.round(ja / (a * b));
+    result = Math.round(factorial(balls)/ (factorial(balls-share) * factorial(share)))
+    return result;   
+    
 }
 solution(5, 3);
