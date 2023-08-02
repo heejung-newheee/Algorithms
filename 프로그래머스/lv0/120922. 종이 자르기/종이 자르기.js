0ddd0760f5133f364e52f9fa608b88a688e78ed9
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-02 09:16:48 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
@@ -1,7 +1,9 @@
 function solution(M, N) {
-    var answer = 0;
-    let width = M - 1;
-    let height = M * (N - 1);
-    answer = width + height;
-    return answer;
+    // var answer = 0;
+    // let width = M - 1;
+    // let height = M * (N - 1);
+    // answer = width + height;
+    // return answer;
+    
+    return answer = M*N-1;
 }
