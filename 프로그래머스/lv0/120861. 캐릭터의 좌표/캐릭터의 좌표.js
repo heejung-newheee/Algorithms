ed6dfb53fdf45358d2763c8d54f314514364cd97
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-04 10:41:03 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,47 @@
+function solution(keyinput, board) {
+    var answer = [0,0];
+    // right(x), up(y)  = +
+    // left(x), down(y)  = -
+    // board-1/2까지
+    let mapX = (board[0]-1)/2
+    let mapY = (board[1]-1)/2
+      
+    
+    // for(i=0; i<keyinput.length; i++){
+    //     if(keyinput[i] === "right"){
+    //         answer[0]+=1
+    //     }else if(keyinput[i] === "left"){
+    //         answer[0]-=1
+    //     }
+    // }      
+    // for(i=0; i<keyinput.length; i++ ){
+    //    if(keyinput[i] === "down"){
+    //         answer[1]-=1
+    //     }else if(keyinput[i] === "up"){
+    //         answer[1]+=1
+    //     }
+    // }
+    
+     keyinput.forEach(i=>{
+        switch(i) {
+            case "right" :
+                if(answer[0] < mapX) answer[0]+=1
+                break;
+            case "left" :
+                if(answer[0] > -mapX) answer[0]-=1
+                break;
+            case 'up' :
+                if(answer[1] < mapY) answer[1]+=1
+                break;
+            case 'down' :
+                if(answer[1] > -mapY) answer[1]-=1
+                break;  
+            default:
+                break;
+        }
+     })
+        
+    return answer;
+}
+
+solution(["down", "down", "down", "down", "down"],[7, 9])
