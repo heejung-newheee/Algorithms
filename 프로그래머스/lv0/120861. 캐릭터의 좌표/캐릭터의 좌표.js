341f8faa04701dffcea273f42b901401588fb5c1
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-04 10:48:04 +0900

    Update 캐릭터의 좌표.js

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,44 +1,42 @@
 function solution(keyinput, board) {
     var answer = [0,0];
-    // right(x), up(y)  = +
-    // left(x), down(y)  = -
-    // board-1/2까지
     let mapX = (board[0]-1)/2
     let mapY = (board[1]-1)/2      
     
-    for(i=0; i<keyinput.length; i++){
-        if(keyinput[i] === "right" && answer[0] < mapX){
-            answer[0]+=1
-        }else if(keyinput[i] === "left" && answer[0] > -mapX){
-            answer[0]-=1
-        }else if(keyinput[i] === "up" && answer[1] < mapY){
-            answer[1]+=1
-        }else if(keyinput[i] === "down" && answer[1] > -mapY){
-            answer[1]-=1
-        }else {
-            continue;
-        }
-    }
+    // for문
+    // for(i=0; i<keyinput.length; i++){
+    //     if(keyinput[i] === "right" && answer[0] < mapX){
+    //         answer[0]+=1
+    //     }else if(keyinput[i] === "left" && answer[0] > -mapX){
+    //         answer[0]-=1
+    //     }else if(keyinput[i] === "up" && answer[1] < mapY){
+    //         answer[1]+=1
+    //     }else if(keyinput[i] === "down" && answer[1] > -mapY){
+    //         answer[1]-=1
+    //     }else {
+    //         continue;
+    //     }
+    // }
     
-    //switch 가 쪼~금 더 빠른듯
-     // keyinput.forEach(i => {
-     //    switch(i) {
-     //        case "right" :
-     //            if(answer[0] < mapX) answer[0]+=1
-     //            break;
-     //        case "left" :
-     //            if(answer[0] > -mapX) answer[0]-=1
-     //            break;
-     //        case 'up' :
-     //            if(answer[1] < mapY) answer[1]+=1
-     //            break;
-     //        case 'down' :
-     //            if(answer[1] > -mapY) answer[1]-=1
-     //            break;  
-     //        default:
-     //            break;
-     //    }
-     // })
+    //switch문 조금 더 빠른듯
+     keyinput.forEach(i => {
+        switch(i) {
+            case "right" :
+                if(answer[0] < mapX) answer[0]+=1
+                break;
+            case "left" :
+                if(answer[0] > -mapX) answer[0]-=1
+                break;
+            case 'up' :
+                if(answer[1] < mapY) answer[1]+=1
+                break;
+            case 'down' :
+                if(answer[1] > -mapY) answer[1]-=1
+                break;  
+            default:
+                break;
+        }
+     })
         
     return answer;
 }
