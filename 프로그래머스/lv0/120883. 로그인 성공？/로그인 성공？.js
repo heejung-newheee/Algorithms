a8172d7d2c0848a36cd77162ff0f7b9d2ce0157f
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-08 10:22:56 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,15 @@
+function solution(id_pw, db) {
+    let answer = "fail"
+    for (let i = 0; i < db.length; i++) {
+        if (db[i][0] === id_pw[0]) {
+            if (db[i][1] === id_pw[1]) {
+                answer =  "login";
+                break;
+            } else {
+                answer = "wrong pw";    
+            } 
+        } 
+    }    
+    
+    return answer;
+}
