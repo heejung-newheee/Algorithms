536c32979414f642df625dbca36b8aea179d67e3
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-09 09:41:05 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,9 +1,8 @@
 function solution(order) {
     var answer = 0;
-    let newOrder = order.toString().split('')
-    console.log(newOrder)
-    for(i=0; i<newOrder.length; i++){        
+    let newOrder = order.toString().split('');
     
+    for(i=0; i<newOrder.length; i++){
       if(newOrder[i] === '3' || newOrder[i] === '6' || newOrder[i] === '9'){
           answer++
       }
