a4526bec86f0753326f5ccd4f2360f4d8030fe16
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-11 09:29:47 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var answer = '';
+    let arr = my_string.split("")
+    for(i=0; i<arr.length; i++){
+            if(answer.includes(arr[i])){
+                continue
+            }else{
+                answer+=arr[i]
+            }
+         
+    }
+    return answer;
+}
