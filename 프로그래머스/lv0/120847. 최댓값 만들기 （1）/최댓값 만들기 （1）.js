6c2bc2fbf21a70ccece4eca72f528f5dc7a73cd6
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-22 02:52:45 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,6 +1,4 @@
 function solution(numbers) {
-    var answer = 0;
-    const arr = numbers.sort((a,b)=> a - b ).reverse();
-    
+    const arr = numbers.sort((a,b)=> a - b ).reverse();    
     return arr[0]*arr[1]
 }
