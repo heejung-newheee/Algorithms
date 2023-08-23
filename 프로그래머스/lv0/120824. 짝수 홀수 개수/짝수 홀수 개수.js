6a3e77de1ab354d9f72003a3bd7c3ccc326ca45d
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-23 20:51:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -3,11 +3,7 @@ function solution(num_list) {
     let even = 0;
     let odd = 0;
     for(i=0; i<num_list.length; i++) {
-        if(num_list[i] %2 === 0){
-            even++
-        }else {
-            odd++
-        }        
+        num_list[i] %2 === 0?even++ : odd++
     }
     answer[0] = even;
     answer[1] = odd;
