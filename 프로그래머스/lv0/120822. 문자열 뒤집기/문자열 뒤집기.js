044360029351356a7910c223c5f02a03470d0014
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-23 20:39:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,5 @@
 function solution(my_string) {
-    let arr = my_string.split('').reverse().join('')
+    // let arr = my_string.split('').reverse().join('')
+    let arr = [...my_string].reverse().join('')
     return arr;
 }
