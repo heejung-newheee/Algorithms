e750ca6147547d3718311ab6f69a48cefd68545f
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-08-24 20:53:00 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,11 +1,12 @@
 function solution(array, n) {
     var answer = 0;
-    for(i=0; i<array.length; i++){
-        if(array[i]===n){
-            answer ++
-        }else{
-            continue;
-        }
-    }
-    return answer;
+    // for(i=0; i<array.length; i++){
+    //     if(array[i]===n){
+    //         answer ++
+    //     }else{
+    //         continue;
+    //     }
+    // }
+    const arr = array.filter((i)=>i === n)
+    return arr.length;
 }
