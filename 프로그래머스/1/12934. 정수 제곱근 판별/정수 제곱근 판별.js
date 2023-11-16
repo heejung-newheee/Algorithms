25410e515be28c196a822bfaea30b94d3f4cccb4
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-11-16 20:22:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    const sqrtValue = Math.sqrt(n);
-
-    // 정수 제곱근을 확인하여 정수인 경우에만 조건을 만족하도록 수정
-    if (Number.isInteger(sqrtValue)) {
-        return Math.pow(sqrtValue + 1, 2);
-    } else {
-        return -1;
-    }
+    // const sqrtValue = Math.sqrt(n);
+    // if (Number.isInteger(sqrtValue)) {
+    //     return Math.pow(sqrtValue + 1, 2);
+    // } else {
+    //     return -1;
+    // }
+    
+    return Math.sqrt(n) === parseInt(Math.sqrt(n)) ? (Math.sqrt(n) + 1) ** 2 : -1
 }
