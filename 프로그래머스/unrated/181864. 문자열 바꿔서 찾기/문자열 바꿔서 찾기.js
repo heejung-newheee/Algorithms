d64ec93672b874c4cd56cbdbd4003b466597f185
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-12-04 22:41:56 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,9 +1,11 @@
 function solution(myString, pat) {
-    var answer = 0;
-    let arr = [...myString].map((item)=> {
-        if(item === 'A') return "B"
-        if(item === 'B') return "A"
-    })
-    if(arr.join("").includes(pat)) return 1;
-    else return 0;
+//     let arr = [...myString].map((item)=> {
+//         if(item === 'A') return "B"
+//         if(item === 'B') return "A"
+//     })
+//     if(arr.join("").includes(pat)) return 1;
+//     else return 0;
+    
+    
+    return [...myString].map(v => v === 'A' ? 'B' : 'A').join('').includes(pat) ? 1 : 0
 }
