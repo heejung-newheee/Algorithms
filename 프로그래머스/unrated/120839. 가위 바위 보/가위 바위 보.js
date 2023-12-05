d70c1a478492ce588ced0ea92342f0ae03f4690d
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-12-05 18:40:42 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
@@ -1,21 +1,35 @@
 function solution(rsp) {
-    let str = [...rsp].map((v)=> {
-        switch(v) {
-          case '2':
-            return '0'
-            break;
+//     let str = [...rsp].map((v)=> {
+//         switch(v) {
+//           case '2':
+//             return '0'
+//             break;
 
-          case '0':  
-            return '5'
-            break;
+//           case '0':  
+//             return '5'
+//             break;
 
-        case '5':  
-            return '2'
-            break;
+//         case '5':  
+//             return '2'
+//             break;
 
-          default:
-            break;
-        }
-    })
-    return str.join('');
+//           default:
+//             break;
+//         }
+//     })
+//     return str.join('');
+    
+    // 삼항연산자
+    return rsp.split("").map((v) => v==="2" ? 0 : (v==="0" ? 5 : 2)).join("")
+    
+    // 객체사용
+    // let arr = {
+    //     2: 0,
+    //     0: 5,
+    //     5: 2
+    // };
+    // var answer = [...rsp].map(v => arr[v]).join("");
+    // return answer;
+    
+    
 }
