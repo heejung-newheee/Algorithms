0cd870e0f167118796dad565a9ff0c9bf1fa9b12
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-12-06 19:14:57 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,5 +1,13 @@
 function solution(a, b) {
-    if(a%2!==0 && b%2 !==0) return (a*a) + (b*b)
-    if(a%2!==0 || b%2 !==0) return 2 * (a+b)
-    if(a%2===0 && b%2 ===0) return Math.abs(a-b)
+    // if(a%2!==0 && b%2 !==0) return (a*a) + (b*b)
+    // if(a%2!==0 || b%2 !==0) return 2 * (a+b)
+    // if(a%2===0 && b%2 ===0) return Math.abs(a-b)
+    
+    const isOdd = (num) => num % 2 === 1;
+
+    return isOdd(a) && isOdd(b)
+        ? a ** 2 + b ** 2
+        : isOdd(a) || isOdd(b)
+        ? 2 * (a + b)
+        : Math.abs(a - b);
 }
