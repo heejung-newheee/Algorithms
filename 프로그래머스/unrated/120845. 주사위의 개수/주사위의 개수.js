61e4aeb906cf0ee2c04bebf96303f4d5223774ce
newheee <126348461+heejung-newheee@users.noreply.github.com>
2023-12-23 23:19:09 +0900

    [level unrated] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
@@ -1,4 +1,6 @@
 function solution(box, n) {
-    let [width, length, height] = box;
-    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+    // let [width, length, height] = box;
+    // return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+    
+    return box.reduce((acc,v) => acc * Math.floor(v / n), 1);
 }
