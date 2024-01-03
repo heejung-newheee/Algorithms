bec434b76c4b8b5df0b7ca539fdbe38ab2b68c08
newheee <126348461+heejung-newheee@users.noreply.github.com>
2024-01-03 10:59:49 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,5 +1,8 @@
 function solution(my_string) {
-    const regex = /[^0-9]/g;
-    const result = my_string.replace(regex, "");
-    return result.split("").map(Number).sort()
+//     const regex = /[^0-9]/g;
+//     const result = my_string.replace(regex, "");
+//     return result.split("").map(Number).sort();
+    
+    
+    return my_string.split("").filter((v) => !isNaN(v)).map((v) => v*1).sort((a,b) => a-b)
 }
