ce94737c855c6c2c126d923ef875c1492dce1863
newheee <126348461+heejung-newheee@users.noreply.github.com>
2024-01-08 19:44:57 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.18 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -3,10 +3,12 @@ function solution(n) {
     for (i=0; i<n; i++) {
         answer.push(Array(n).fill(0));
     }
-    console.log(answer)
     for (i=0; i<answer.length; i++) {
+        // answer 배열의 각 행에 대해서 반복
         const item = answer[i];
+        // 현재 행을 나타내는 배열을 가져옴
         answer[i][i] = 1;
+        // 현재 행의 대각선에 있는 요소를 1로 설정
     }
 
     return answer;
